7dcc5ab55b325166c242e98f49c73644b12979ec
KamolineR <164529564+KamolineR@users.noreply.github.com>
2024-06-25 12:25:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const numeroAleatorio = Math.floor(Match.random() * 10) + 1;
+const inputGuess = document.getElementById('palpite');
+const buttonSubmit = document.getElementById('submit');
+const messageDiv = document.getElementById('mensagem');
+let tentativas = 0;
+
+function verificarPalpite() {
+    const palpite = Number(inputGuess.value);
+    tentativas++;
+
+    if (palpite === numeroAleatorio) {
+        messageDiv.textContent = 'Parabéns! Você acertou em $(tentativas) tentativa(s)'
+        messageDiv.style.color = 'green';
+        setTimeout(() => {
+            window.location.reload();
+        }, 5000)
+    } else if (palpite < numeroAleatorio){
+        messageDiv.textContent = 'Tente novamente. Você chutou o numero $(palpite)';
+        messageDiv.style.color = 'orange';
+    }
+}
